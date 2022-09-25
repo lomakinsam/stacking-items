415fa2b5c22697ff835521583b2dc4ed309c8f2d
Lomakin Semen <lomakin.semyon.olegovich@gmail.com>
2022-09-25 19:57:43 +0300

    Stable SecondOrderDemo.cs

--- Assets/Stacking/Scripts/SecondOrderDemo.cs
+++ Assets/Stacking/Scripts/SecondOrderDemo.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 using Unity.Mathematics;
+using System.Collections.Generic;
 
 public class SecondOrderDemo : MonoBehaviour
 {
@@ -10,41 +11,48 @@ public class SecondOrderDemo : MonoBehaviour
     [Range(0, 2)]
     public float Z;
 
-    [Range(-1, 1)]
+    [Range(-10, 10)]
     public float R;
 }
 
 [CustomEditor(typeof(SecondOrderDemo))]
 public class TestOnInspector : Editor
 {
-    private Material mat;
-
     private const float defaultLenght = 2.0f;
     private const float defaultValue = 1.0f;
-
-    private const int drawSteps = 300;
 
     private const float paddingLeft = 10f;
     private const float paddingRight = 2f;
     private const float paddingTop = 15f;
     private const float paddingBottom = 15f;
 
+    private const int evaluationSteps = 300;
+
     private float f, f0, z, z0, r, r0;
 
     private SecondOrderDynamics func;
-   
-    private Vector2[] data;
+
+    private Material mat;
+
+    private EvaluationData evalData;
 
     private void OnEnable()
     {
         var shader = Shader.Find("Hidden/Internal-Colored");
         mat = new Material(shader);
 
+        evalData = new();
+
         InitFunction();
     }
 
     private void OnDisable()
     {
+        func = null;
+        evalData = null;
+
+        f = f0 = z = z0 = r = r0 = float.NaN;
+
         DestroyImmediate(mat);
     }
 
@@ -62,6 +70,12 @@ public class TestOnInspector : Editor
             GL.Clear(true, false, Color.black);
             mat.SetPass(0);
 
+            float rectWidth = rect.width - paddingLeft - paddingRight;
+            float rectHeight = rect.height - paddingTop - paddingBottom;
+
+            float x_AxisOffset = rectHeight * math.remap(evalData.Y_min, evalData.Y_max, 0, 1, 0);
+            float defaultValueOffset = rectHeight * math.remap(evalData.Y_min, evalData.Y_max, 0, 1, 1); ;
+
             // draw base graph
             GL.Begin(GL.LINES);
             GL.Color(new Color(1, 1, 1, 1));
@@ -69,55 +83,54 @@ public class TestOnInspector : Editor
             GL.Vertex3(paddingLeft, paddingTop, 0);
             GL.Vertex3(paddingLeft, rect.height - paddingBottom, 0);
             // draw X axis
-            GL.Vertex3(paddingLeft, rect.height - paddingBottom, 0);
-            GL.Vertex3(rect.width - paddingRight, rect.height - paddingBottom, 0);
+            GL.Vertex3(paddingLeft, rect.height - x_AxisOffset - paddingBottom, 0);
+            GL.Vertex3(rect.width - paddingRight, rect.height - x_AxisOffset - paddingBottom, 0);
             // draw default values
             GL.Color(Color.green);
-            //GL.Vertex3(paddingLeft, paddingTop, 0);
-            //GL.Vertex3(rect.width - paddingRight, paddingTop, 0);
+            GL.Vertex3(paddingLeft, rect.height - defaultValueOffset - paddingBottom, 0);
+            GL.Vertex3(rect.width - paddingRight, rect.height - defaultValueOffset - paddingBottom, 0);
             GL.End();
 
-            // init func values
-            if (data == null)
-            {
-                Debug.Log("Init");
-                data = new Vector2[drawSteps];
+            // evaluate func values
+            if (evalData.IsEmpty) EvaluateFunction();
 
-                float rectWidth = rect.width - paddingLeft - paddingRight;
-                float rectHeight = rect.height - paddingTop - paddingBottom;
-
-                UpdateData(rectWidth, rectHeight);
-            }
-
-            // validate input changes
+            // re-evaluate func values after input values changed
             if (f != f0 || z != z0 || r != r0)
             {
-                Debug.Log("Value changed");
                 InitFunction();
-
-                float rectWidth = rect.width - paddingLeft - paddingRight;
-                float rectHeight = rect.height - paddingTop - paddingBottom;
-
-                UpdateData(rectWidth, rectHeight);
+                EvaluateFunction();
             }
 
             // draw graph
             GL.Begin(GL.LINE_STRIP);
             GL.Color(Color.cyan);
-            for (int i = 0; i < data.Length; i++)
-                GL.Vertex3(data[i].x + paddingLeft, rect.height - (data[i].y + paddingTop), 0.0f);
+            for (int i = 0; i < evalData.Length; i++)
+            {
+                Vector2 point = evalData.GetItem(i);
+
+                float x_remap = math.remap(evalData.X_min, evalData.X_max, 0, rectWidth, point.x);
+                float y_remap = math.remap(evalData.Y_min, evalData.Y_max, 0, rectHeight, point.y);
+
+                GL.Vertex3(paddingLeft + x_remap, rect.height - y_remap - paddingBottom, 0.0f);
+            }
             GL.End();
 
             GL.PopMatrix();
             GUI.EndClip();
-        }
 
-        // draw values
-        rect = GUILayoutUtility.GetLastRect();
-        float squareSize = 10;
-        EditorGUI.LabelField(new Rect(rect.x + paddingLeft - squareSize, rect.y + paddingTop - squareSize/2, squareSize, squareSize), "1"); // heigt "1" mark
-        EditorGUI.LabelField(new Rect(rect.x + paddingLeft - squareSize, rect.y + rect.height + (squareSize * 0.2f) - paddingBottom, squareSize, squareSize), "0"); // height "0" mark
-        EditorGUI.LabelField(new Rect(rect.x + rect.width - paddingRight - squareSize, rect.y + rect.height + (squareSize * 0.2f) - paddingBottom, squareSize, squareSize), "2"); // max lenght mark
+            // draw values
+            float squareSize = 10;
+            EditorGUI.LabelField(new Rect(rect.x + paddingLeft - squareSize, rect.y + rect.height - defaultValueOffset - paddingBottom - squareSize/2, squareSize, squareSize), "1"); // heigt "1" mark
+            EditorGUI.LabelField(new Rect(rect.x + paddingLeft - squareSize, rect.y + rect.height - x_AxisOffset - paddingBottom + (squareSize * 0.2f), squareSize, squareSize), "0"); // height "0" mark
+            EditorGUI.LabelField(new Rect(rect.x + rect.width - paddingRight - squareSize, rect.y + rect.height - x_AxisOffset - paddingBottom + (squareSize * 0.2f) , squareSize, squareSize), "2"); // max lenght mark
+        }
+    }
+
+    private void UpdateInput()
+    {
+        f = ((SecondOrderDemo)target).F;
+        z = ((SecondOrderDemo)target).Z;
+        r = ((SecondOrderDemo)target).R;
     }
 
     private void InitFunction()
@@ -129,51 +142,23 @@ public class TestOnInspector : Editor
         func = new SecondOrderDynamics(f, z, r, new Vector3(-defaultLenght, 0, 0));
     }
 
-    private void UpdateData(float graphWidth, float graphHeight)
+    private void EvaluateFunction()
     {
-        float x_min = 0, x_max = 0, y_min = 0, y_max = 0;
+        evalData.Clear();
 
-        for (int i = 0; i < drawSteps; i++)
+        for (int i = 0; i < evaluationSteps; i++)
         {
-            float T = 0.0069f;
-            float x = math.remap(0, drawSteps - 1, -defaultLenght, defaultLenght, i);
-            float y = x > 0 ? defaultValue : 0;
+            float T = 0.016f; // constant deltaTime (60 frames per second)
 
-            Vector3? funcValues = func.Update(T, new Vector3(x, y, 0));
+            // input step function params
+            float x_input = math.remap(0, evaluationSteps - 1, -defaultLenght, defaultLenght, i);
+            float y_input = x_input > 0 ? defaultValue : 0;
 
-            if (i == 0)
-            {
-                x_min = x_max = funcValues.Value.x;
-                y_min = y_max = funcValues.Value.y;
-            }
-            else
-            {
-                x_max = funcValues.Value.x > x_max ? funcValues.Value.x : x_max;
-                x_min = funcValues.Value.x < x_min ? funcValues.Value.x : x_min;
+            Vector3? funcValues = func.Update(T, new Vector3(x_input, y_input, 0));
 
-                y_max = funcValues.Value.y > y_max ? funcValues.Value.y : y_max;
-                y_min = funcValues.Value.y < y_min ? funcValues.Value.y : y_min;
-            }
+            if (x_input <= 0) continue; // data is gathered only after the Y value has changed
 
-
-            data[i] = new Vector2(funcValues.Value.x, funcValues.Value.y);
+            evalData.Add(new Vector2(funcValues.Value.x, funcValues.Value.y));
         }
-
-        for (int i = 0; i < drawSteps; i++)
-        {
-            float x = math.remap(x_min, x_max, 0, graphWidth, data[i].x);
-            float y = math.remap(y_min, y_max, 0, graphHeight, data[i].y);
-
-            data[i] = new Vector2(x, y);
-        }
-
-        Debug.Log($"X limits: [{x_min}; {x_max}]\nY limits [{y_min}; {y_max}]");
-    }
-
-    private void UpdateInput()
-    {
-        f = ((SecondOrderDemo)target).F;
-        z = ((SecondOrderDemo)target).Z;
-        r = ((SecondOrderDemo)target).R;
     }
 }
--- Assets/Stacking/Scripts/SecondOrderDynamics.cs
+++ Assets/Stacking/Scripts/SecondOrderDynamics.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using Unity.Mathematics;
 
 public class SecondOrderDynamics
 {
     private Vector3? xp;
     private Vector3? y, yd;
-    private float k1, k2, k3;
+    private float _w, _z, _d, k1, k2, k3;
 
     public SecondOrderDynamics(float f, float z, float r, Vector3 x0)
     {
-        k1 = z / (Mathf.PI * f);
-        k2 = 1 / ((2 * Mathf.PI * f) * (2 * Mathf.PI * f));
-        k3 = r * z / (2 * Mathf.PI * f);
+        _w = 2 * math.PI * f;
+        _z = z;
+        _d = _w * math.sqrt(math.abs(z * z - 1));
+        k1 = z / (math.PI * f);
+        k2 = 1 / (_w * _w);
+        k3 = r * z / _w;
 
         xp = x0;
         y = x0;
@@ -25,8 +29,24 @@ public class SecondOrderDynamics
             xp = x;
         }
 
+        float k1_stable, k2_stable;
+        if (_w * T < _z)
+        {
+            k1_stable = k1;
+            k2_stable = Mathf.Max(k2, T * T / 2 + T * k1 / 2, T * k1);
+        }
+        else
+        {
+            float t1 = math.exp(-_z * _w * T);
+            float alpha = 2 * t1 * (_z <= 1 ? math.cos(T * _d) : math.cosh(T * _d));
+            float beta = t1 * t1;
+            float t2 = T / (1 + beta - alpha);
+            k1_stable = (1 - beta) * t2;
+            k2_stable = T * t2;
+        }
+
         y = y + T * yd;
-        yd = yd + T * (x + k3 * xd - y - k1 * yd) / k2;
+        yd = yd + T * (x + k3 * xd - y - k1_stable * yd) / k2_stable;
         return y;
     }
 }
